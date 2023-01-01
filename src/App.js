7d79bebd6c5b8314356bbdf4acd7ce74a2814b86
matinsangabi <sangabiwork@gmail.com>
2023-01-01 12:30:39 -0800

    home page user cart added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import { Routes , Route } from 'react-router-dom';
 import Homepage from './pages/Home';
 function App() {
   return (
-    <>
+    <div className='w-full h-screen bg-gray-100'>
       <Routes>
         <Route path='/' element={<Homepage/>} />
       </Routes>
-    </>
+    </div>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,32 @@
 const Homepage = () => {
     return ( 
-        <div className="max-w-screen-xl mx-auto container" >
+        <div className="max-w-screen-xl mx-auto container">
             <div className="w-full flex items-center justify-between pt-10">
                 <input type="search" placeholder="جستجو" className="p-2 rounded-md border-none outline-none ring-1 focus:ring-offset-2 ring-gray-500 transition-all ease-in-out duration-500" /> 
                 <div className="p-2">مرتب سازی</div>
+            </div>
+            <div className="grid grid-cols-12 pt-6 gap-6">
+                <div className="col-span-6 py-4 px-2 bg-[#BFEAF5] rounded-md shadow-md text-[#0A2647] flex flex-col">
+                    <div className="flex items-center gap-x-2 justify-between">
+                        <h1 className="font-semibold"> نام :متین اسدسنگابی</h1>
+                        <h1 className="font-semibold"> شماره پلاک : #</h1>
+                        <h1 className="font-semibold"> تلفن :09178694033</h1>
+                    </div>
+                </div>
+                <div className="col-span-6 py-4 px-2 bg-[#BFEAF5] rounded-md shadow-md text-[#0A2647] flex flex-col">
+                    <div className="flex items-center gap-x-2 justify-between">
+                        <h1 className="font-semibold"> نام :متین اسدسنگابی</h1>
+                        <h1 className="font-semibold"> شماره پلاک : #</h1>
+                        <h1 className="font-semibold"> تلفن :09178694033</h1>
+                    </div>
+                </div>
+                <div className="col-span-6 py-4 px-2 bg-[#BFEAF5] rounded-md shadow-md text-[#0A2647] flex flex-col">
+                    <div className="flex items-center gap-x-2 justify-between">
+                        <h1 className="font-semibold"> نام :متین اسدسنگابی</h1>
+                        <h1 className="font-semibold"> شماره پلاک : #</h1>
+                        <h1 className="font-semibold"> تلفن :09178694033</h1>
+                    </div>
+                </div>
             </div>
         </div>
      );
