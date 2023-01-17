dbd9e748be740c0e7052c0e00f66188dcd1fd6d8
matinsangabi <sangabiwork@gmail.com>
2023-01-17 12:38:23 -0800

    layout and header added

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -24,7 +24,7 @@ const SignIn = () => {
   });
   return (
     <div className="w-full h-screen flex items-center justify-center">
-      <div className="bg-gray-100 rounded-md ring-2 shadow-lg p-2 ring-gray-500">
+      <div className="bg-gray-100 rounded-md ring-2 shadow-lg p-4 ring-gray-500">
         <form
           className="flex items-center flex-col gap-y-4"
           onSubmit={formik.handleSubmit}
@@ -34,7 +34,7 @@ const SignIn = () => {
           })}
           <button
             type="submit"
-            className="p-2 rounded-md bg-[#198278] text-gray-100 hover:ring hover:ring-[#198278] hover:ring-offset-2 transition-all ease-in-out duration-300"
+            className="p-2 w-full rounded-md bg-[#198278] text-gray-100 hover:ring hover:ring-[#198278] hover:ring-offset-2 transition-all ease-in-out duration-300"
           >
             ثبت نام{" "}
           </button>
